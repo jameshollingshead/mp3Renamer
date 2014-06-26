46f5a080f6b61390bd7f77c14eba0064a8b05a3f
James Hollingshead <git@jameshollingshead.com>
2014-06-26 13:36:08 -0400

    Added functions to get PWD and list of Mp3s from PWD

---
+++ Mp3Renamer/Mp3Renamer/Mp3Services.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mp3Renamer
+{
+    public static class Mp3Services
+    {
+        public static string GetPresentWorkingDirectory()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        public static IEnumerable<string> GetMp3FileList(string currentDirectory)
+        {
+            var result = Directory.EnumerateFiles(currentDirectory, "*.*", SearchOption.TopDirectoryOnly)
+            .Where(s => s.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase));
+
+            return result;
+        }
+    }
+}
